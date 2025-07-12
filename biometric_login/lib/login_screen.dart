e621ebb067e659af248914d882583c73a741225b
roychandu <cr717753@gmail.com>
2025-07-12 22:12:17 +0530

    check Biometrics and isDeviceSupported method

--- biometric_login/lib/login_screen.dart
+++ biometric_login/lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -7,7 +8,33 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
+void _checkDeviceSupport() async {
+  final LocalAuthentication auth = LocalAuthentication();
+  bool canCheckBiometrics = await auth.canCheckBiometrics;
+  bool isDeviceSupported = await auth.isDeviceSupported();
+
+  if (isDeviceSupported) {
+    print('Device supports biometrics');
+    List<BiometricType> biometrics = await auth.getAvailableBiometrics();
+    print(biometrics);
+    if (biometrics.isNotEmpty) {
+      print('Available biometrics: $biometrics');
+    } else {
+      print('No biometrics available');
+    }
+  } else {
+    print('Device does not support biometrics');
+  }
+}
+
 class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _checkDeviceSupport();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold());
